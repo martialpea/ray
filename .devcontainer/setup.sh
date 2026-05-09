822631e85b71b94f6edca67a7376c3441538a1e1
martialpea <mail1@na8.ir>
2026-05-09 21:26:14 +0330

    Add files via upload

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,5 +9,9 @@ curl -sL "$RELEASE" -o "$TMPDIR/xray.zip"
 unzip -q "$TMPDIR/xray.zip" -d "$TMPDIR"
 install -m 755 "$TMPDIR/xray" /usr/local/bin/xray
 
+echo "[g2ray] Downloading GeoIP and GeoSite..."
+curl -sL "https://github.com/v2fly/geoip/releases/latest/download/geoip.dat" -o /usr/local/bin/geoip.dat
+curl -sL "https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat" -o /usr/local/bin/geosite.dat
+
 rm -rf "$TMPDIR"
-echo "[g2ray] Xray installed successfully."
+echo "[g2ray] Done."
